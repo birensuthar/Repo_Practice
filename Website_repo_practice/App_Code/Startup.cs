3750690365ce8d316b7a06f8aba56541590124f7
Biren Suthar <bsuthar2@studentmail.nait.ca>
2016-09-12 16:18:18 -0600

    Create new repo from VS project

---
+++ Website_repo_practice/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Website_repo_practice.Startup))]
+namespace Website_repo_practice
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
